09f5fb1a387b154a13b8aaacf09fcb13920ee821
Aradhya Shukla <aradhya1703@gmail.com>
2024-12-14 19:25:32 +0530

    portfolio web

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Aradhya",
+  description: "Portfolio of Aradhya Shukla",
+  author: "Aradhya",
+  url: "https://vaibd.vercel.app",
+  ogImage: "https://vaibd.vercel.app/og.jpg",
+  links: {
+    github: "https://github.com/vaibd/minimalFolio",
+  },
+};
---
+++ constants/about.ts
@@ -0,0 +1,135 @@
+export const myName = "Aradhya Shukla";
+
+export const hero = {
+  heading: "Aradhya Shukla.",
+  heading2: "Full-Stack Developer and Tech Enthusiast.",
+  content:
+    "As a developer, I specialize in creating scalable and innovative solutions. With hands-on experience in full-stack development, data science, and DevOps, I enjoy pushing the boundaries of technology.",
+};
+
+export const startDate = "August 1, 2021";
+
+export const about = {
+  heading: "Hi, I am Aradhya Shukla!",
+  professionalInfo:
+    "I thrive on building efficient, scalable, and user-friendly web applications. My expertise spans across frameworks like React, Django, and Node.js, along with cloud services like AWS and Azure. I am eager to collaborate on challenging projects that push boundaries.",
+  personalInfo:
+    "My journey began as an Electronics and Communication Engineering student, where I developed a passion for Python and JavaScript. I soon delved into full-stack development, machine learning, and DevOps, honing my skills with hands-on projects and internships.",
+  casualLife:
+    "When I'm not coding, you can find me documenting my life through mini vlogs, working out, or exploring new tech trends. I also enjoy watching anime, movies, and occasionally stargazing or diving into astrophotography.",
+};
+
+export const skills = [
+  {
+    name: "ReactJS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    name: "NextJS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+  },
+  {
+    name: "NodeJS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "TypeScript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "Python",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  },
+  {
+    name: "Linux",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg",
+  },
+  {
+    name: "Docker",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+  },
+  {
+    name: "Django",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+  },
+  {
+    name: "Flask",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg",
+  },
+  {
+    name: "Azure",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg",
+  },
+  {
+    name: "AWS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+  },
+];
+
+export const timeline = [
+  {
+    year: 2024,
+    events: [
+      {
+        title: "Software Developer Intern",
+        subTitle: "MXDP Limited",
+        icon: "DesktopIcon",
+      },
+    ],
+  },
+  {
+    year: 2023,
+    events: [
+      {
+        title: "Content Creator, Frontend Developer",
+        subTitle: "True Brands",
+        icon: "BrushIcon",
+      },
+    ],
+  },
+  {
+    year: 2021,
+    events: [
+      {
+        title: "B.E. Electronics and Communication",
+        subTitle: "Bharati Vidyapeeth College of Engineering",
+        icon: "BackpackIcon",
+        type: "education",
+      },
+    ],
+  },
+  {
+    year: 2019,
+    events: [
+      {
+        title: "Senior Secondary",
+        subTitle: "Vikhe Patil Memorial School",
+        icon: "BackpackIcon",
+        type: "education",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    name: "Email",
+    url: "mailto:aradhya1703@gmail.com",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/aradhyashukla/",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/Alfastrek",
+  },
+];
